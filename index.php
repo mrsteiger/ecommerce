9a3afcddbd68f70ccc015f244803a084f08c2609
Martin Steiger <mrsteiger@gmail.com>
2021-04-05 12:26:59 -0300

    template e page admin

--- index.php
+++ index.php
@@ -2,18 +2,26 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Mrsteiger\Page;
+use \Mrsteiger\PageAdmin;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$sql = new Mrsteiger\DB\Sql();
-	
-	$results = $sql->select("SELECT * FROM tb_users");
-	
-	echo json_encode($results);
+	$page = new Page();
 
+	$page->setTpl("index");
+});
+
+$app->get('/admin', function() {
+    
+	$page = new PageAdmin();
+
+	$page->setTpl("index");
 });
 
 $app->run();
